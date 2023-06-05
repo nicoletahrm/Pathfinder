ff19c95c6f3419b867f04bd017feb3fa8a42f7e6
Nicoleta Hermeneanu <hermeneanunicoleta14@yahoo.com>
2023-06-05 21:14:45 +0300

    Polyline from draw to walking on map

--- pathfinder_app/lib/screens/map_screen.dart
+++ pathfinder_app/lib/screens/map_screen.dart
@@ -2,10 +2,8 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../api/api_key.dart';
 import '../widgets/custom_nav_bar.dart';
 import '../controllers/global_controller.dart';
 import 'dart:convert';
@@ -29,7 +27,6 @@ class MapScreenState extends State<MapScreen> {
 
   late LatLng sourceLocation;
   late LatLng destination;
-  PolylinePoints polylinePoints = PolylinePoints();
   List<LatLng> polylineCoordinates = [];
   Set<Marker> markers = {};
 
@@ -75,50 +72,19 @@ class MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
+    PolylineId polylineId = PolylineId('route');
+
     Set<Polyline> polylines = {
       Polyline(
-        polylineId: PolylineId('route'),
-        points: [
-          LatLng(45.4941053, 25.4744011),
-          LatLng(45.4929622, 25.4737145),
-          LatLng(45.4916987, 25.4740578),
-          LatLng(45.4904953, 25.4731137),
-          LatLng(45.488991, 25.4737145),
-          LatLng(45.4881486, 25.474487),
-          LatLng(45.4877274, 25.474487),
-          LatLng(45.4876071, 25.4751736),
-          LatLng(45.4863434, 25.4754311),
-          LatLng(45.4859824, 25.4748303),
-          LatLng(45.4828532, 25.4738861),
-          LatLng(45.482432, 25.4747445),
-          LatLng(45.4814691, 25.4751736),
-          LatLng(45.4808071, 25.4761177),
-          LatLng(45.4799044, 25.4748303),
-          LatLng(45.480446, 25.4732853),
-          LatLng(45.4803257, 25.472427),
-          LatLng(45.4795433, 25.472427),
-          LatLng(45.4796035, 25.4714829),
-          LatLng(45.4782193, 25.4718262),
-          LatLng(45.4772564, 25.4697663),
-          LatLng(45.4762934, 25.4698521),
-          LatLng(45.472923, 25.4627282),
-          LatLng(45.47196, 25.4624707),
-          LatLng(45.471659, 25.4614407),
-          LatLng(45.4700941, 25.4615265),
-          LatLng(45.4702145, 25.4601532),
-          LatLng(45.469131, 25.4593808),
-          LatLng(45.4664825, 25.4568058),
-          LatLng(45.4664825, 25.4559475),
-          LatLng(45.4670242, 25.4549176),
-          LatLng(45.4658805, 25.4535443)
-        ],
-        color: Colors.pink,
-        width: 6,
-      ),
+          polylineId: polylineId,
+          points: polylineCoordinates,
+          color: Colors.pink,
+          width: 4,
+          startCap: Cap.roundCap,
+          endCap: Cap.roundCap),
     };
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Pathfinder')),
       body: SafeArea(
         child: Column(
           children: [
@@ -131,7 +97,6 @@ class MapScreenState extends State<MapScreen> {
                 zoomControlsEnabled: true,
                 onMapCreated: (GoogleMapController mapController) {
                   mapController = mapController;
-                  getPolyPoints();
                 },
                 myLocationEnabled: true,
               ),
@@ -145,18 +110,8 @@ class MapScreenState extends State<MapScreen> {
 
   void getPolyPoints() async {
     polylineCoordinates = await extractCoordinatesFromKmlFile(
-      'assets/images/cabana_malaiesti.kml',
+      'assets/files/cabana_malaiesti2.kml',
     );
-
-    if (polylineCoordinates.isNotEmpty) {
-      polylineCoordinates.forEach((LatLng point) {
-        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
-      });
-    }
-
-    // polylineCoordinates = await extractCoordinatesFromKmlFile(
-    //   'assets/images/cabana_malaiesti.kml',
-    // );
 
     setState(() {});
   }
@@ -200,8 +155,6 @@ class MapScreenState extends State<MapScreen> {
     } catch (e) {
       print('Error extracting coordinates: $e');
     }
-
-    print("POLYLINES: " + coordinates.toString());
 
     return coordinates;
   }
